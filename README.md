6e598a97e574428cd0ee59ec1bf78b546693132b
Dennis1000 <dennis@spreendigital.de>
2016-06-30 14:03:29 +0200

    Updated GPLv3 license info in the header files

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-LibBLP v1.0 - A BLP image reader library with DXT3/5 decompression support
+# LibBLP v1.0 
+A BLP image reader library with DXT3/5 decompression support
 for Delphi 10.1 Berlin+ by Dennis Spreen
 http://blog.spreendigital.de/2016/06/16/libblp/
+
+
+**Uses code from Nihlus/libwarcraft https://github.com/Nihlus/libwarcraft**
+
 
 (c) Copyrights 2016 Dennis D. Spreen <dennis@spreendigital.de>
 
 
 
-Licensing
+# Licensing
 
 LibBLP is Free Software, and is distributed under the GPLv3 license. This means, in simple terms, that you are free to do whatever you want with the source code and any binaries compiled or generated from it as long as you pass on those rights to anyone aquiring a copy of the source code or binaries. The full licence can be read in the file "LICENSE" at the root of the source tree, or at http://choosealicense.com/licenses/gpl-3.0/, where a more people-friendly summary is also available.
--- Source/LibBLP.DXT.pas
+++ Source/LibBLP.DXT.pas
@@ -3,21 +3,16 @@
   http://blog.spreendigital.de/2016/06/16/libblp/
 
   (c) Copyrights 2016 Dennis D. Spreen <dennis@spreendigital.de>
-  This unit is free and can be used for any needs. The introduction of
-  any changes and the use of those changed library is permitted without
-  limitations. Only requirement:
-  This text must be present without changes in all modifications of library.
 
-  * The contents of this file are used with permission, subject to
-  * the Mozilla Public License Version 1.1 (the "License"); you may   *
-  * not use this file except in compliance with the License. You may  *
-  * obtain a copy of the License at                                   *
-  * http:  www.mozilla.org/MPL/MPL-1.1.html                           *
-  *                                                                   *
-  * Software distributed under the License is distributed on an       *
-  * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or    *
-  * implied. See the License for the specific language governing      *
-  * rights and limitations under the License.                         *
+  Uses GPLv3 licensed code from Nihlus/libwarcraft (https://github.com/Nihlus/libwarcraft)
+
+  LibBLP is Free Software, and is distributed under the GPLv3 license. 
+  This means, in simple terms, that you are free to do whatever you want
+  with the source code and any binaries compiled or generated from it as 
+  long as you pass on those rights to anyone aquiring a copy of the source
+  code or binaries. The full licence can be read in the file "LICENSE" at 
+  the root of the source tree, or at http://choosealicense.com/licenses/gpl-3.0/, 
+  where a more people-friendly summary is also available
 }
 unit LibBLP.DXT;
 
